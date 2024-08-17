19f58008254e90b12179416ac6c7feae2ac4c276
Иван Стрельцов <vanya.streltsov@gmail.com>
2024-08-17 23:57:20 +0300

    base

---
+++ main.py
@@ -0,0 +1,151 @@
+import kivy
+kivy.require('2.3.0')  # replace with your current kivy version!
+
+from kivy.uix.accordion import Animation
+from kivy.uix.accordion import NumericProperty
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+from kivy.animation import Animation
+from kivy.metrics import dp, sp
+from kivy.clock import Clock
+import time
+
+
+class MenuScreen(Screen):
+    def test(self):
+        print('Click!')
+        self.ids.title.text = "New Title"
+        pass
+
+
+class SettingsScreen(Screen):
+    pass
+
+
+class ShopScreen(Screen):
+    pass
+
+
+class GameScreen(Screen):
+    pass
+
+
+class CreditsScreen(Screen):
+    pass
+
+
+class RebirthScreen(Screen):
+    pass
+
+
+class ObjectGame(Image):
+    anim_play = False
+
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.pos[0], touch.pos[1]) and self.anim_play == False:
+            self.anim_play = True
+            app.points = app.points + app.number
+
+            temp_x = self.x
+            temp_y = self.y
+            temp_width = self.width
+            temp_height = self.height
+            temp_center = self.center
+
+            anim = Animation(width=dp(300), duration=0.03) + Animation(width=temp_width, duration=0.03)
+            anim &= Animation(height=dp(300), duration=0.03) + Animation(height=temp_height, duration=0.03)
+            anim &= Animation(center=self.parent.center, duration=0.03) + Animation(center=self.parent.center, duration=0.03)
+
+            anim.start(self)
+            anim.bind(on_complete=lambda a, b: setattr(self, 'anim_play', False))
+
+            print('Touchy!')
+        else:
+            print('Owchie!')
+        if app.points == 50:
+            ...
+        return super().on_touch_down(touch)
+
+class Upgrade1(Button):
+    anim_play = False
+    price = NumericProperty(50)
+    def on_touch_down(self, touch):
+
+        if self.collide_point(touch.pos[0], touch.pos[1]) and self.anim_play == False and app.points >= self.price:
+            app.number = app.number * 2
+            app.points -= self.price
+            app.ball = 'images/greatball.png'
+
+            self.price *= 2.5
+
+            print('Upgrade!')
+        else:
+            print('No Upgrade?')
+        return super().on_touch_down(touch)
+
+
+class Upgrade2(Button):
+    anim_play = False
+    
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.pos[0], touch.pos[1]) and self.anim_play == False and app.points >= 250:
+            app.number = app.number * 2
+            app.points -= 250
+            app.ball = 'images/ultraball.png'
+
+            print('Upgrade!')
+        else:
+            print('No Upgrade?')
+        return super().on_touch_down(touch)
+
+
+class Upgrade3(Button):
+    anim_play = False
+    price = NumericProperty(50)
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.pos[0], touch.pos[1]) and self.anim_play == False and app.points >= self.price:
+            app.number2 += 1
+            app.points -= 100
+
+            self.price *= 2
+            print(app.number2)
+        else:
+            print('No Upgrade?')
+        return super().on_touch_down(touch)
+
+
+class PokeClicker(App):
+    points = NumericProperty(0) 
+    number = 1
+    number2 = 0
+    ball = 'images/pokeball.png'
+
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='main'))
+        sm.add_widget(GameScreen(name="game"))
+        sm.add_widget(ShopScreen(name="shop"))
+        sm.add_widget(CreditsScreen(name="credits"))
+        sm.add_widget(SettingsScreen(name="settings"))
+
+        # с помощью модуля
+        Clock.schedule_interval(self.update_points, 1)
+
+        return sm
+
+    def update_points(self, dt):
+        self.points += self.number2
+
+
+# Window.clearcolor = (1, 1, 1, 1)
+
+if __name__ == '__main__':
+    Window.size = (450, 900)
+    app = PokeClicker()
+    app.run()
